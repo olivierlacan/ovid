b60c8bf56878a91eb42b7c187ec6e72c95d1cd0f
Olivier Lacan <hi@olivierlacan.com>
2020-03-29 17:11:40 -0400

    Add California counties parser

---
+++ parser_ca.rb
@@ -0,0 +1,47 @@
+require "net/http"
+require "json"
+require "csv"
+require "date"
+
+county_data_uri = URI "https://services1.arcgis.com/P5Mv5GY5S66M8Z1Q/arcgis/rest/services/NCOV_Cases_California_Counties/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+county_data_raw = Net::HTTP.get(county_data_uri)
+county_data_json = JSON.parse(county_data_raw)
+
+cases = county_data_json["features"]
+# {
+# OBJECTID: 4,
+# COUNTY_NAME: "San Benito",
+# FIPS: "06069",
+# POPULATION: 59354,
+# POP_SQMI: 42.7,
+# Active: 11,
+# Deaths: 1,
+# Recovered: 2,
+# Cumulative: 14,
+# Shape__Area: 5599403013.33594,
+# Shape__Length: 458142.190640416
+# }
+
+testing_keys = %w[
+  cumulative
+  deaths
+  population
+  positive
+  recovered
+]
+
+testing_totals = testing_keys.each_with_object({}) do |key, store|
+  store[key] = 0
+end
+
+cases.each_with_object(testing_totals) do |test, store|
+  a = test["attributes"]
+
+  store["cumulative"] += a["Cumulative"]
+  store["deaths"] += a["Deaths"]
+  store["population"] += a["POPULATION"]
+  store["positive"] += a["Active"]
+  store["recovered"] += a["Recovered"]
+end
+
+pp testing_totals

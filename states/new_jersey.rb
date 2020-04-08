fa3f98aad7158d2530786afeca5b48448133a592
Olivier Lacan <hi@olivierlacan.com>
2020-04-07 20:35:40 -0400

    Fix NJ

--- states/new_jersey.rb
+++ states/new_jersey.rb
@@ -22,7 +22,11 @@ class NewJersey < State
   end
 
   def self.testing_data_url
-    "https://services7.arcgis.com/Z0rixLlManVefxqY/arcgis/rest/services/DailyCaseCounts/FeatureServer/0//query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+    "https://services7.arcgis.com/Z0rixLlManVefxqY/arcgis/rest/services/DailyCaseCounts/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+  end
+
+  def self.hospital_data_url
+    "https://services7.arcgis.com/Z0rixLlManVefxqY/ArcGIS/rest/services/NJ_Beds_By_Counties/FeatureServer/0?f=json"
   end
 
   def self.dashboard_url
@@ -42,18 +46,29 @@ class NewJersey < State
     # Shape__Area: 1436707422.2616,
     # Shape__Length: 381356.570662874
     {
+      new_positives: {
+        name: "New Positives",
+        description: "Tallied from individual county cases.",
+        source: "NEW_CASES"
+      },
+      new_deaths: {
+        name: "New Deaths",
+        description: "Tallied from individual county cases.",
+        source: "NEW_DEATHS"
+      },
       positives: {
-        name: "Positives (Counties)",
+        name: "Positives",
         description: "Tallied from individual county cases.",
         highlight: true,
         source: "TOTAL_CASES"
       },
       deaths: {
-        name: "Deaths (Counties)",
+        name: "Deaths",
         description: "Tallied from individual county cases.",
         highlight: true,
-        source: "DEATHS"
+        source: "TOTAL_DEATHS"
       }
+
     }
   end
 end

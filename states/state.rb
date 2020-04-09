07498e71afb2c7faaa5ff4aa772ef906c7af48d5
Olivier Lacan <hi@olivierlacan.com>
2020-04-09 04:52:18 -0400

    Refactor State class for aggregates & cases

--- states/state.rb
+++ states/state.rb
@@ -25,15 +25,15 @@ class State
     "covid_#{state_name.downcase}"
   end
 
-  def self.testing_gallery_url
+  def self.counties_feature_url
     nil
   end
 
-  def self.testing_feature_url
+  def self.cases_feature_url
     nil
   end
 
-  def self.testing_data_url
+  def self.aggregates_feature_url
     nil
   end
 
@@ -41,102 +41,257 @@ class State
     nil
   end
 
-  def self.aggregate_feature_url
+  def self.county_keys
     nil
   end
 
-  def self.aggregate_data_url
+  def self.case_keys
     nil
   end
 
-   def self.covid_tracking_report(query_string)
-    stored_response = check_cache(cache_key)
+  def self.totals_keys
+    nil
+  end
+
+  def self.county_cache_key
+    "#{cache_key}-county-report"
+  end
+
+  def self.case_cache_key
+    "#{cache_key}-case-report"
+  end
+
+  def self.totals_cache_key
+    "#{cache_key}-totals-report"
+  end
+
+  def self.county_report(query_string)
+    stored_response = check_cache(county_cache_key)
 
     if stored_response && !query_string.include?("reload")
       puts "Using stored_response..."
       stored_response
     else
       puts "Generating new report ..."
-      get_data
+      get_county_data
     end
   end
 
-  def self.last_edit
-    puts "Fetching lastEditDate from feature layer #{testing_feature_url} ..."
-    uri = URI(testing_feature_url)
-    response = Net::HTTP.get(uri)
-    parsed_response = JSON.parse(response)
-    puts parsed_response.inspect
-    raw_edit_date = parsed_response["editingInfo"]["lastEditDate"]
+  def self.case_report(query_string)
+    stored_response = check_cache(case_cache_key)
+
+    if stored_response && !query_string.include?("reload")
+      puts "Using stored_response..."
+      stored_response
+    else
+      puts "Generating new report ..."
+      get_case_data
+    end
+  end
+
+  def self.totals_report(query_string)
+    stored_response = check_cache(totals_cache_key)
+
+    if stored_response && !query_string.include?("reload")
+      puts "Using stored_response..."
+      stored_response
+    else
+      puts "Generating new report ..."
+      get_totals_data
+    end
+  end
+
+  def self.get(url, params = {})
+    params = params.merge({ f: "pjson" })
+    uri = URI(url)
+    uri.query = URI.encode_www_form(params)
+    puts "Sending GET request to #{uri} ..."
+    response = Net::HTTP.get_response(uri)
+    if response.is_a?(Net::HTTPSuccess)
+      puts "Success!"
+      JSON.parse(response.body)
+    else
+      raise "#{response.code}: #{response.message}"
+      puts "Headers: #{res.to_hash.inspect}"
+      puts res.body if response.response_body_permitted?
+    end
+  end
+
+  def self.last_edit(metadata)
+    raw_edit_date = metadata["editingInfo"]["lastEditDate"]
     converted_edit_date = Time.strptime(raw_edit_date.to_s, "%Q")
     puts "Parsed lastEditDate #{raw_edit_date} converted to #{converted_edit_date} (#{ENV["TZ"]})"
 
     converted_edit_date
   end
 
-  def self.request(url)
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
+  def self.get_totals_data
+    if totals_keys
+      metadata = get(totals_feature_url)
+      last_edit = last_edit(metadata)
 
-    JSON.parse(response)
-  end
+      query = {
+        where: "1=1",
+        returnGeometry: false,
+        outFields: "*",
+        resultType: "standard"
+      }
 
-  def self.get_data
-    if defined?(relevant_keys)
-      relevant_response = request(testing_data_url)
-      relevant_report = generate_roll_up_report(relevant_response["features"])
+      response = get("#{totals_feature_url}/query", query)
+
+      totals_report = generate_totals_report(
+        response["features"],
+        initialize_store(totals_keys)
+      )
     end
 
-    if defined?(aggregate_keys)
-      aggregate_response = request(aggregate_data_url)
-      aggregate_report = generate_aggregate_report(aggregate_response["features"])
-    end
-
-    # set expiration time to 15 minutes from now
-    last_fetch = Time.now
-    expiration_time = last_fetch + (CACHE_EXPIRATION_IN_MINUTES * 60)
-
-    if defined?(aggregate_report) && !aggregate_report.nil?
-      merged_data = relevant_report.merge(aggregate_report)
-    else
-      merged_data = relevant_report
-    end
-
-    cache = {
-      last_edited_at: last_edit.iso8601,
-      last_fetched_at: last_fetch.iso8601,
-      expires_at: expiration_time.iso8601,
-      data: merged_data
+    merged_data = {
+      edited_at: last_edit,
+      fetched_at: Time.now,
+      data: totals_report
     }
 
-    write_cache(cache_key, cache)
-    set_expiration(cache_key, expiration_time)
+    save_in_cache totals_cache_key, merged_data
+  end
+
+  def self.get_county_data
+    if county_keys
+      metadata = get(counties_feature_url)
+      last_edit = last_edit(metadata)
+
+      query = {
+        where: "1=1",
+        returnGeometry: false,
+        outFields: "*",
+        resultType: "standard"
+      }
+
+      response = get("#{counties_feature_url}/query", query)
+
+      county_report = generate_county_report(
+        response["features"],
+        initialize_store(county_keys)
+      )
+    end
+
+    merged_data = {
+      edited_at: last_edit,
+      fetched_at: Time.now,
+      data: county_report
+    }
+
+
+    save_in_cache county_cache_key, merged_data
+  end
+
+  def self.total_records_from_feature(url, query)
+    get("#{url}/query", query.merge({ returnCountOnly: true }))["count"]
+  end
+
+  def self.get_case_data
+    if cases_feature_url
+      metadata = get(cases_feature_url)
+      last_edit = last_edit(metadata)
+
+      maximum_record_count = metadata["standardMaxRecordCount"]
+
+      query = {
+        where: "1=1",
+        returnGeometry: false,
+        outFields: "*",
+        resultRecordCount: maximum_record_count,
+        resultType: "standard"
+      }
+
+      record_total = total_records_from_feature(cases_feature_url, query)
+      puts "Total records: #{record_total}"
+
+      initial_response = get("#{cases_feature_url}/query", query)
+      puts "Records in initial response: #{initial_response["features"].count}"
+
+      last_item_id = initial_response["features"].last["attributes"]["ObjectId"]
+
+      @response_data = []
+      @response_data.push(*initial_response["features"])
+
+      # we need to iterate because the maximum record count sent back per
+      # request is lower than the absolute total number of record.
+      if record_total > maximum_record_count
+        puts "Iterating through #{record_total} records to retrieve all ..."
+        while last_item_id < record_total do
+          puts "current offset: #{last_item_id}"
+          response = get("#{cases_feature_url}/query", query.merge(resultOffset: last_item_id))
+          puts "Count of results: #{response["features"].count}"
+          @response_data << response["features"]
+
+          last_item_id = response["features"].last["attributes"]["ObjectId"]
+        end
+      else
+        puts "All records (#{record_total}) can be fetched in a single request!"
+      end
+
+      case_report = generate_case_report(
+        @response_data,
+        initialize_store(case_keys)
+      )
+
+      merged_data = {
+        edited_at: last_edit,
+        fetched_at: Time.now,
+        data: case_report
+      }
+
+      save_in_cache case_cache_key, merged_data
+    end
+  end
+
+  def self.save_in_cache(cache_key, data, expiration_time = Time.now)
+    write_cache(cache_key, data)
+    set_expiration(cache_key, Time.now + (CACHE_EXPIRATION_IN_MINUTES * 60))
     check_cache(cache_key)
   end
 
-  def self.generate_roll_up_report(data)
-    testing_store = initialize_store(relevant_keys)
-    data.each_with_object(testing_store) do |test, store|
-      a = test["attributes"]
+  def self.generate_totals_report(data, store)
+    a = data.first["attributes"]
 
-      relevant_keys.each do |key, value|
+    totals_keys.each_with_object(store) do |(key, value), store|
+      if value[:total]
+        store[key][:value] = a[value[:source]]
+      else
+        store[key][:value] += a[value[:source]] || 0
+      end
+    end
+  end
+
+  def self.generate_county_report(data, store)
+    data.each_with_object(store) do |item, memo|
+      a = item["attributes"]
+
+      county_keys.each do |key, value|
         if value[:total]
-          store[key][:value] = a[value[:source]]
+          memo[key][:value] = a[value[:source]]
         else
-          store[key][:value] += a[value[:source]] || 0
+          memo[key][:value] += a[value[:source]] || 0
         end
       end
     end
   end
 
-  def self.generate_aggregate_report(data)
-    aggregate_store = initialize_store(aggregate_keys)
+  def self.generate_case_report(data, store)
+    data.each_with_object(store) do |item, store|
+      a = item["attributes"]
 
-    data.each_with_object(aggregate_store) do |data, store|
-      a = data["attributes"]
-
-      aggregate_keys.each do |key, value|
-        store[key][:value] = a[value[:source]] || "N/A"
+      case_keys.each do |key, value|
+        if value[:count_of_total_records]
+          store[key][:value] = data.count
+        elsif value[:total]
+          store[key][:value] = a[value[:source]]
+        elsif value[:positive_value]
+          positive_value = a[value[:source]] == value[:positive_value]
+          store[key][:value] += 1 if positive_value
+        else
+          store[key][:value] += a[value[:source]] || 0
+        end
       end
     end
   end
@@ -151,10 +306,6 @@ class State
         source: metric[:source]
       }
     end
-  end
-
-  def self.relevant_keys
-    raise NotImplementedError
   end
 
   def self.production?
